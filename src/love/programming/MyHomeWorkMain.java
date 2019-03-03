c8e02d81152a1ffd0bd66f28a6da00b8c7457cef
jyothipaul@yahoo.com <jyothipaul@yahoo.com>
2019-03-03 21:31:35 +0000

    Executing home work

---
+++ src/love/programming/MyHomeWorkMain.java
@@ -0,0 +1,10 @@
+package love.programming;
+
+public class MyHomeWorkMain {
+    public static void main(String[] args) {
+
+
+        MyHomeWorkClass1 myHomeWorkClass1Object1 = new MyHomeWorkClass1();
+        MyHomeWorkClass2 myHomeWorkClass2Object2 = new MyHomeWorkClass2();
+    }
+}
